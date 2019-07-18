1fa482a792c541242ea21097c22ee1fbb8e32665
nand0san <hancaidolosdos@hotmail.com>
2019-07-19 00:39:28 +0200

    autoparseo funcionando bien, al parecer ES no permite autoparsear nada mas que un tipo de csv cada vez

--- stop.sh
+++ stop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker-compose down
+#docker-compose down
+#docker rmi $(docker images -q)
+docker kill $(docker ps -a -q)
+docker system prune -f
+docker network prune -f
 docker volume prune -f
